48c0859e2da0e986a0f18d67cded798e448f4bf9
strokatov <strokatov91@mail.ru>
2022-07-12 15:11:05 +0300

    Agricultures class

---
+++ base_objects/agriculture.py
@@ -0,0 +1,6 @@
+class Agriculture:
+    def __init__(self, volume: int, cost_price, grain_price):
+        self.volume = volume
+
+        self.cost_price = cost_price
+        self.grain_price = grain_price
